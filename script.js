ac80a87a95c3988e086be24bcda0e39d6608de86
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2025-03-01 12:43:53 +0300

    style : mordern look

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ document
   .getElementById("toggle-dark-mode")
   .addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
+    this.classList.toggle("dark");
+
+    const icon = this.querySelector("i");
+    if (document.body.classList.contains("dark-mode")) {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    } else {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    }
   });
+
+document.body.classList.add("dark-mode");
+toggleBtn.classList.add("dark");
+icon.classList.remove("fa-sun");
+icon.classList.add("fa-moon");
